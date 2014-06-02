ce9f1f2112e541bde23f636fca3310c994c5cdae
Gary Ewan Park <gep13@gep13.co.uk>
2014-06-02 08:43:52 +0100

    ng-click and ng-repeat

---
+++ clients/ng/scripts/HelloController.js
@@ -0,0 +1,18 @@
+﻿var HelloController = function($scope)
+{
+    $scope.showMessage = function () {
+        $scope.message = "Hello, World!";
+    }
+}
+
+
+
+var MoviesController = function ($scope) {
+    var movies = [
+        { title: "Star Wars", length: 120, released: 1981 },
+    { title: "Top Gun", length: 90, released: 1984 },
+    { title: "Hot Shots", length: 110, released: 1985 }
+    ];
+
+    $scope.movies = movies;
+}
