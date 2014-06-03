395c4837a92de0ea55e6351833b422b359e7ae1e
Gary Ewan Park <gep13@gep13.co.uk>
2014-06-03 11:48:36 +0100

    Before routing

---
+++ clients/ng/scripts/alert.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    var module = angular.module("atTheMovies");
+    module.directive("alert", function () {
+        return {
+            restrict: "EA",
+            transclude: true,
+            templateUrl: "views/alert.html",
+            scope: {
+                type: "@",
+                reason: "=",
+                close: "&"
+            },
+            replace: true,
+            };
+    })
+}());
--- clients/ng/scripts/atTheMovies.js
+++ clients/ng/scripts/atTheMovies.js
@@ -1,5 +1,29 @@
 ﻿(function () {
-    var module = angular.module("atTheMovies", []);
+    var module = angular.module("atTheMovies", ["ngRoute"]);
+
+    module.config(function ($provide) {
+        $provide.decorator("$exceptionHandler", function ($delegate) {
+            return function (reason, cause) {
+                $delegate(reason, cause);
+                alert(reason);
+            }
+        })
+    })
+
+    module.config(function ($routeProvider) {
+        $routeProvider
+            .when("/list", {
+                templateUrl: "/views/list.html",
+                controller: "MovieListController"
+            })
+        .when("/edit/:id", {
+            templateUrl: "/views/edit.html",
+            controller: "MovieEditController"
+        })
+        .otherwise({
+            redirectTo: "/list"
+        });
+    });
 
     module.run(function ($rootScope) {
         $rootScope.appConfig = {
